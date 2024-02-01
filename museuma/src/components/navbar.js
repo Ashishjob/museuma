130dbf5b6439f15847496d4533a3061a6448d02e
Ashish Job <ashishjob104@gmail.com>
2024-02-01 12:27:13 -0600

    feat: add navbar features

--- museuma/src/components/navbar.js
+++ museuma/src/components/navbar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <header class="text-[#313639] body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header class="text-[#313639] body-font flex flex-row justify-evenly">
+      <div class="container ml-20 flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <img src="./logo.svg" alt="logo" className="w-12">
+        </img>
         <a
           href="local"
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
@@ -24,6 +26,14 @@ export default function NavBar() {
             Fourth Link
           </a>
         </nav>
+
+      </div>
+      <div class="parent">
+        <button class="inline-flex justify-center items-center bg-[#EFEDE5] border-0 py-1 px-3 focus:outline-none hover:bg-[#DCD7C5] rounded text-base mt-8 md:mt-0 ml-auto">Log In
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
       </div>
     </header>
   );
