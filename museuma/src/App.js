f7d918338042eaab05f94be1fc3d6aaf381ca83e
Ashshjob <ashishjob104@gmail.com>
2024-02-03 19:03:52 -0600

    feat: add image animation and update cursor

--- museuma/src/App.js
+++ museuma/src/App.js
@@ -1,13 +1,31 @@
 import ThreeJSPage from "./components/threejs";
 import Languages from "./components/languages";
+import { useEffect, useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./App.css";
 import "./index.css";
 
 export default function Home() {
+  const images = [
+    "/museuma1.png",
+    "/museuma2.png",
+    "/museuma3.png",
+    "/museuma4.png",
+    "/museuma5.png",
+  ];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000); // Change image every 5 seconds
+
+    return () => clearInterval(intervalId); // Clean up on unmount
+  }, []);
   return (
     <main className="h-screen bg-[#EFEDE5] w-screen">
       <div className="w-full flex flex-row justify-between">
-        <div className="flex items-center ml-40 -mt-40 w-2/3">
+        <div className="flex ml-40 mt-20 w-2/3">
           <div className="flex flex-col mr-8">
             <Languages />
             <div className="flex">
@@ -16,14 +34,19 @@ export default function Home() {
               </h3>
             </div>
             <div className="flex flex-col">
-              <img
-                src="/museuma1.png"
-                alt="Museuma Logo"
-                width={200}
-                height={200}
-                className="w-full"
-              />
-              <span className="mt-4 text-2xl text-[#313639]">
+              <div className="carousel">
+                {images.map((image, index) => (
+                  <img
+                    key={image}
+                    src={image}
+                    alt="Museuma Logo"
+                    className={`carousel-image ${
+                      index === currentImageIndex ? "visible" : ""
+                    }`}
+                  />
+                ))}
+              </div>
+              <span className="mt-32 text-2xl text-[#313639]">
                 <a href="/exhibitions">View Exhibitions</a>
               </span>
             </div>
