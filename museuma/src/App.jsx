985fc3161be72164a8518f70c2c5d7ad907a3677
e-muffin <edgarmorfin16@gmail.com>
2024-03-20 23:19:00 -0500

    exhibits page

---
+++ museuma/src/App.jsx
@@ -0,0 +1,46 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import Home from "./pages/Home";
+import Dining from "./pages/dining";
+import Complaints from "./pages/complaints";
+import Artworks from "./pages/artworks";
+import Exhibits from "./pages/exhibits"
+import ShoppingCart from "./pages/shoppingCart";
+import Checkout from "./pages/checkout";
+import AdminHome from "./adminPages/Home";
+import ManageEmployees from "./adminPages/manageEmployees";
+import TotalReport from "./adminPages/totalReport";
+import ManageExhibit from "./adminPages/manageExhibits";
+import ManageArtwork from "./adminPages/manageArtworks";
+import ViewComplaints from "./adminPages/viewComplaints";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/complaints" element={<Complaints />} />
+        <Route path="/dining" element={<Dining />} />
+        <Route path="/artworks" element={<Artworks />} />
+        <Route path="/exhibits" element={<Exhibits />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin/manage-employees" element={<ManageEmployees />} />
+        <Route path="/admin/total-report" element={<TotalReport />} />
+        <Route path="/admin/manage-exhibits" element={<ManageExhibit />} />
+        <Route path="/admin/manage-artworks" element={<ManageArtwork />} />
+        <Route path="/admin/view-complaints" element={<ViewComplaints />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ museuma/src/pages/exhibits.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const exhibits = [
+    {
+      title: 'Modern Art: A Retrospective',
+      description: 'Explore the evolution of modern art through this comprehensive retrospective featuring works by renowned artists of the 20th century.',
+      location: 'Gallery 1',
+      image: '/exhibit1.jpg',
+    },
+    {
+      title: 'Ancient Civilizations Unveiled',
+      description: 'Marvel at artifacts, sculptures, and relics that offer glimpses into the rich cultural heritage of bygone eras.',
+      location: 'Gallery 2',
+      image: '/exhibit2.jpg',
+    },
+    {
+        title: "Exploring Nature's Canvas: Landscapes from Around the World",
+        description: "From majestic mountains to serene seascapes, immerse yourself in the beauty and diversity of our natural world captured by talented artists.",
+        location: 'Gallery 3',
+        image: '/exhibit3.jpg',
+    },
+  ];
+
+function Exhibits() {
+    return(
+        <div className="p-4 mx-auto max-w-7xl">
+            <div className="text-4xl font-bold mb-8 text-center">Exhibits</div>
+            <p className="mb-6 text-2xl text-gray-600">
+                Welcome to the heart of artistic discovery at Bakers Museum. 
+                Our exhibits offer a captivating journey through diverse realms of art, history, and culture. 
+                Immerse yourself in a world of creativity, innovation, 
+                and storytelling as you browse through our current and upcoming exhibits.
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {exhibits.map((exhibition, index) => (
+                    <div key={index} className="bg-white rounded-lg overflow-hidden shadow-lg p-6">
+                        
+                        <img src={exhibition.image} alt={`Image for ${exhibition.title}`} className="w-full h-64 object-cover mb-4"/>
+                        <div className="p-4">
+                            <h2 className="text-2xl font-semibold mb-2">{exhibition.title}</h2>
+                            <p className="text-gray-700 mb-4">{exhibition.description}</p>
+                            <p className="text-sm text-gray-500">Location: {exhibition.location}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Exhibits
