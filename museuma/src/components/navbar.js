611bdfce0e706e21d17eba7edfb0bcf9eff58898
Ashshjob <ashishjob104@gmail.com>
2024-03-23 13:59:14 -0500

    feat: add admin login

--- museuma/src/components/navbar.js
+++ museuma/src/components/navbar.js
@@ -4,6 +4,7 @@ export default function NavBar() {
   const [showLoginPopup, setShowLoginPopup] = useState(false);
   const [showSignUpPopup, setShowSignUpPopup] = useState(false);
   const [password, setPassword] = useState("");
+  const [showAdminLoginPopup, setShowAdminLoginPopup] = useState(false);
 
   const handleLoginClick = () => {
     setShowLoginPopup(true);
@@ -23,11 +24,11 @@ export default function NavBar() {
     <header className="text-[#313639] body-font z-1 shadow">
       <div className="w-full flex justify-between items-center pl-5 pt-5 pb-5 pr-2">
         <div className="flex items-center">
-          
           <a
             href="/"
             className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-          ><img src="/logo.svg" alt="logo" className="w-12 mx-10"></img>
+          >
+            <img src="/logo.svg" alt="logo" className="w-12 mx-10"></img>
             <span className="ml-3 text-xl">Baker Museum</span>
           </a>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
@@ -116,6 +117,17 @@ export default function NavBar() {
                 >
                   Sign Up
                 </button>
+                <div className="flex items-center justify-center mt-4 mb-2">
+                  <div className="border-t border-[#DCD7C5] flex-grow"></div>
+                  <p className="px-2 ">Are you an administrator?</p>
+                  <div className="border-t border-[#DCD7C5] flex-grow"></div>
+                </div>
+                <button
+                  onClick={() => setShowAdminLoginPopup(true)}
+                  className="w-full py-2 px-4 bg-[#DCD7C5] text-black rounded hover:bg-[#C4BFAC] mt-2"
+                >
+                  Admin Login
+                </button>
               </div>
             </div>
           )}
@@ -156,6 +168,53 @@ export default function NavBar() {
                       Sign up
                     </button>
                   )}
+                </form>
+              </div>
+            </div>
+          )}
+          {showAdminLoginPopup && (
+            <div className="login-popup z-50 flex items-center justify-center">
+              <div className="login-popup-inner bg-[#EFEDE5] rounded-lg shadow-lg p-8 relative">
+                <button
+                  onClick={() => setShowAdminLoginPopup(false)}
+                  className="absolute top-2 left-2 bg-[#DCD7C5] text-black rounded-full w-6 h-6 flex items-center justify-center hover:bg-[#C4BFAC]"
+                >
+                  X
+                </button>
+                <h2 className="text-2xl font-bold mb-6 text-center text-[#333]">
+                  Admin Login
+                </h2>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    const username = e.target.elements[0].value;
+                    const password = e.target.elements[1].value;
+                    if (username === "admin" && password === "secure123!") {
+                      // Redirect to /admin page
+                      window.location.href = "/admin";
+                    } else {
+                      // Handle incorrect username or password
+                      alert("Incorrect username or password");
+                    }
+                  }}
+                >
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    className="w-full p-2 mb-4 border border-[#DCD7C5] rounded"
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    className="w-full p-2 mb-6 border border-[#DCD7C5] rounded"
+                  />
+
+                  <button
+                    type="submit"
+                    className="w-full py-2 px-4 bg-[#DCD7C5] text-black rounded hover:bg-[#C4BFAC]"
+                  >
+                    Log in
+                  </button>
                 </form>
               </div>
             </div>
