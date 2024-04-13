6640bdfebe14778383cea4f588d52b3cbe10c198
Ashshjob <ashishjob104@gmail.com>
2024-04-12 22:09:16 -0500

    refactor: add env

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "bakermuseum.mysql.database.azure.com",
-    user: 'mrbaker',
-    password: 'Meowmeow!!!',
-    database: 'museum',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
         rejectUnauthorized: true
     }
