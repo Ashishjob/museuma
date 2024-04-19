dd6b30f8f3787347cff95398872f78a6837a4fe4
Jynxae <realkhalily@hotmail.com>
2024-04-19 16:10:50 -0500

    User is directed back to home page after login

--- museuma/src/components/navbar.js
+++ museuma/src/components/navbar.js
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import { HiArchiveBox } from "react-icons/hi2";
-
 import Cookies from "js-cookie";
 
 export default function NavBar() {
@@ -12,7 +11,6 @@ export default function NavBar() {
   const [employeeDepartment, setEmployeeDepartment] = useState("");
   const [hasUnresolvedMessages, setHasUnresolvedMessages] = useState(false);
   const [queue, setQueue] = useState([]);
-
   const [cartQuantity, setCartQuantity] = useState(0);
 
   useEffect(() => {
@@ -287,25 +285,25 @@ const useCart = () => {
             <span className="ml-3 text-xl">Baker Museum</span>
           </a>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-        <a href="/exhibits" className="mr-5 hover:text-gray-900">
-          Exhibits
-        </a>
-        <a href="/artworks" className="mr-5 hover:text-gray-900">
-          Artworks
-        </a>
-        <a href={isLoggedIn ? "/tickets" : "/login"} className="mr-5 hover:text-gray-900">
-          Tickets
-        </a>
-        <a href={isLoggedIn ? "/giftshop" : "/login"} className="mr-5 hover:text-gray-900">
-          Gift Shop
-        </a>
-        <a href={isLoggedIn ? "/dining" : "/login"} className="mr-5 hover:text-gray-900">
-          Dining
-        </a>
-        <a href={isLoggedIn ? "/complaints" : "/login"} className="mr-5 hover:text-gray-900">
-          Report a Problem
-        </a>
-      </nav>
+            <a href="/exhibits" className="mr-5 hover:text-gray-900">
+              Exhibits
+            </a>
+            <a href="/artworks" className="mr-5 hover:text-gray-900">
+              Artworks
+            </a>
+            <a href={isLoggedIn ? "/tickets" : "/login"} className="mr-5 hover:text-gray-900">
+              Tickets
+            </a>
+            <a href={isLoggedIn ? "/giftshop" : "/login"} className="mr-5 hover:text-gray-900">
+              Gift Shop
+            </a>
+            <a href={isLoggedIn ? "/dining" : "/login"} className="mr-5 hover:text-gray-900">
+              Dining
+            </a>
+            <a href={isLoggedIn ? "/complaints" : "/login"} className="mr-5 hover:text-gray-900">
+              Report a Problem
+            </a>
+          </nav>
           <div className="relative">
 
           <HiArchiveBox className="text-2xl cursor-pointer" onClick={togglePopup} />
