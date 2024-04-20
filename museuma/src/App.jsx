14468c6fbcbf4b03c93b3f8c665d18edd5572fdf
Ashshjob <ashishjob104@gmail.com>
2024-04-20 16:46:22 -0500

    refactor: fix day report and remove admin login

--- museuma/src/App.jsx
+++ museuma/src/App.jsx
@@ -25,7 +25,6 @@ import ExhibitReport from "./adminPages/exhibitReport";
 import ManageRestaurant from "./adminPages/manageRestaurant";
 import Login from "./components/login";
 import Signup from "./components/signup";
-import AdminLogin from "./components/adminLogin";
 import ThanksForOrder from "./pages/thanksForOrder";
 import ComplaintConfirmed from "./pages/complaintConfirmed";
 
@@ -100,7 +99,6 @@ function App() {
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/thanks-for-order" element={<ThanksForOrder />} />
         <Route path="/complaint-confirmed" element={<ComplaintConfirmed />} />
         <Route
