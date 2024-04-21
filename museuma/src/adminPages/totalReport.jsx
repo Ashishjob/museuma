e8eda8e52c917b866149be802ec427d35c7caaaa
Jynxae <realkhalily@hotmail.com>
2024-04-20 19:28:02 -0500

    ignore

--- museuma/src/adminPages/totalReport.jsx
+++ museuma/src/adminPages/totalReport.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
-import moment from 'moment';
+import moment from "moment";
 import "../App.css";
 import "../index.css";
 
@@ -11,7 +11,7 @@ function TotalReport() {
   const [searchTerm, setSearchTerm] = useState("");
 
   // Add this state to keep track of the selected report type
-  const [reportType, setReportType] = useState('totalSales');
+  const [reportType, setReportType] = useState("totalSales");
 
   // Add this function to handle changes to the report type
   const handleReportTypeChange = (event) => {
@@ -19,11 +19,11 @@ function TotalReport() {
   };
 
   const [sortField, setSortField] = useState(null);
-  const [sortDirection, setSortDirection] = useState('asc');
+  const [sortDirection, setSortDirection] = useState("asc");
 
   const handleHeaderClick = (field) => {
     setSortField(field);
-    setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+    setSortDirection(sortDirection === "asc" ? "desc" : "asc");
   };
 
   const sortedData = React.useMemo(() => {
@@ -33,7 +33,7 @@ function TotalReport() {
       if (a[sortField] > b[sortField]) return 1;
       return 0;
     });
-    return sortDirection === 'asc' ? sorted : sorted.reverse();
+    return sortDirection === "asc" ? sorted : sorted.reverse();
   }, [salesData, sortField, sortDirection]);
 
   useEffect(() => {
@@ -43,14 +43,14 @@ function TotalReport() {
   const fetchSalesData = async () => {
     let url;
     switch (reportType) {
-      case 'giftShop':
-        url = 'https://museuma.onrender.com/gift-shop-report';
+      case "giftShop":
+        url = "https://museuma.onrender.com/gift-shop-report";
         break;
-      case 'tickets':
-        url = 'https://museuma.onrender.com/ticket-report';
+      case "tickets":
+        url = "https://museuma.onrender.com/ticket-report";
         break;
       default:
-        url = 'https://museuma.onrender.com/total-report';
+        url = "https://museuma.onrender.com/total-report";
     }
     try {
       const response = await fetch(url);
@@ -71,13 +71,13 @@ function TotalReport() {
   }, [reportType]);
 
   // Add these states to keep track of the search terms and date filter
-  const [customerNameSearchTerm, setCustomerNameSearchTerm] = useState('');
-  const [itemBoughtSearchTerm, setItemBoughtSearchTerm] = useState('');
-  const [dateFilter, setDateFilter] = useState('all');
+  const [customerNameSearchTerm, setCustomerNameSearchTerm] = useState("");
+  const [itemBoughtSearchTerm, setItemBoughtSearchTerm] = useState("");
+  const [dateFilter, setDateFilter] = useState("all");
   const [currentFilters, setCurrentFilters] = useState({
-    customerName: '',
-    itemBought: '',
-    dateFilter: 'all',
+    customerName: "",
+    itemBought: "",
+    dateFilter: "all",
   });
 
   const [startDate, setStartDate] = useState(null);
@@ -103,21 +103,32 @@ function TotalReport() {
 
   // Modify your filter function to filter by customer_name, item_bought, and date
   const filteredData = salesData?.filter((sale) => {
-    const customerNameMatches = sale?.customer_name?.toLowerCase().includes(customerNameSearchTerm.toLowerCase());
-    const itemBoughtMatches = sale?.item_bought?.toLowerCase().includes(itemBoughtSearchTerm.toLowerCase());
+    const customerNameMatches = sale?.customer_name
+      ?.toLowerCase()
+      .includes(customerNameSearchTerm.toLowerCase());
+    const itemBoughtMatches = sale?.item_bought
+      ?.toLowerCase()
+      .includes(itemBoughtSearchTerm.toLowerCase());
     let dateMatches;
     switch (dateFilter) {
-      case 'lastWeek':
-        dateMatches = moment(sale?.date).isAfter(moment().subtract(1, 'weeks'));
+      case "lastWeek":
+        dateMatches = moment(sale?.date).isAfter(moment().subtract(1, "weeks"));
         break;
-      case 'lastMonth':
-        dateMatches = moment(sale?.date).isAfter(moment().subtract(1, 'months'));
+      case "lastMonth":
+        dateMatches = moment(sale?.date).isAfter(
+          moment().subtract(1, "months")
+        );
         break;
-      case 'lastYear':
-        dateMatches = moment(sale?.date).isAfter(moment().subtract(1, 'years'));
+      case "lastYear":
+        dateMatches = moment(sale?.date).isAfter(moment().subtract(1, "years"));
         break;
-      case 'between':
-        dateMatches = moment(new Date(sale?.order_date)).isBetween(moment(new Date(startDate)), moment(new Date(endDate)), undefined, '[]');
+      case "between":
+        dateMatches = moment(new Date(sale?.order_date)).isBetween(
+          moment(new Date(startDate)),
+          moment(new Date(endDate)),
+          undefined,
+          "[]"
+        );
         break;
       default:
         dateMatches = true;
@@ -125,23 +136,37 @@ function TotalReport() {
     return customerNameMatches && itemBoughtMatches && dateMatches;
   });
 
-  const customerCounts = filteredData ? filteredData.reduce((counts, sale) => {
-    counts[sale.customer_name] = (counts[sale.customer_name] || 0) + 1;
-    return counts;
-  }, {}) : {};
+  const customerCounts = filteredData
+    ? filteredData.reduce((counts, sale) => {
+        counts[sale.customer_name] = (counts[sale.customer_name] || 0) + 1;
+        return counts;
+      }, {})
+    : {};
 
-  const mostActiveCustomer = Object.keys(customerCounts).reduce((a, b) => customerCounts[a] > customerCounts[b] ? a : b, 'N/A');
+  const mostActiveCustomer = Object.keys(customerCounts).reduce(
+    (a, b) => (customerCounts[a] > customerCounts[b] ? a : b),
+    "N/A"
+  );
 
-  const itemTotals = filteredData ? filteredData.reduce((totals, sale) => {
-    totals[sale.item_bought] = (totals[sale.item_bought] || 0) + 1;
-    return totals;
-  }, {}) : {};
+  const itemTotals = filteredData
+    ? filteredData.reduce((totals, sale) => {
+        totals[sale.item_bought] = (totals[sale.item_bought] || 0) + 1;
+        return totals;
+      }, {})
+    : {};
 
-  const mostPopularItem = Object.keys(itemTotals).reduce((a, b) => itemTotals[a] > itemTotals[b] ? a : b, 'N/A');
+  const mostPopularItem = Object.keys(itemTotals).reduce(
+    (a, b) => (itemTotals[a] > itemTotals[b] ? a : b),
+    "N/A"
+  );
 
-  const totalAmountSpent = filteredData ? filteredData.reduce((total, sale) => total + sale.total_price, 0) : 0;
+  const totalAmountSpent = filteredData
+    ? filteredData.reduce((total, sale) => total + sale.total_price, 0)
+    : 0;
 
-  const totalQuantity = filteredData ? filteredData.reduce((total, sale) => total + sale.quantity_bought, 0) : 0;
+  const totalQuantity = filteredData
+    ? filteredData.reduce((total, sale) => total + sale.quantity_bought, 0)
+    : 0;
 
   const numRowsShowing = filteredData ? filteredData.length : 0;
 
@@ -160,17 +185,34 @@ function TotalReport() {
         </h1>
 
         <div className="mb-4 flex justify-center space-x-4">
-        <select value={reportType} onChange={handleReportTypeChange} className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none">
-          <option value="totalSales">Total Sales Report</option>
-          <option value="giftShop">Gift Shop Sales Report</option>
-          <option value="tickets">Ticket Sales Report</option>
-        </select>
-          <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-            type="text" value={customerNameSearchTerm} onChange={handleCustomerNameSearchTermChange} placeholder="Search by customer name" />
-          <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-            type="text" value={itemBoughtSearchTerm} onChange={handleItemBoughtSearchTermChange} placeholder="Search by item bought" />
-          <select className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-            value={dateFilter} onChange={handleDateFilterChange}>
+          <select
+            value={reportType}
+            onChange={handleReportTypeChange}
+            className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          >
+            <option value="totalSales">Total Sales Report</option>
+            <option value="giftShop">Gift Shop Sales Report</option>
+            <option value="tickets">Ticket Sales Report</option>
+          </select>
+          <input
+            className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="text"
+            value={customerNameSearchTerm}
+            onChange={handleCustomerNameSearchTermChange}
+            placeholder="Search by customer name"
+          />
+          <input
+            className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="text"
+            value={itemBoughtSearchTerm}
+            onChange={handleItemBoughtSearchTermChange}
+            placeholder="Search by item bought"
+          />
+          <select
+            className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            value={dateFilter}
+            onChange={handleDateFilterChange}
+          >
             <option value="all">All dates</option>
             <option value="lastWeek">Last week</option>
             <option value="lastMonth">Last month</option>
@@ -223,15 +265,23 @@ function TotalReport() {
                   <td className="px-4 py-2 border">{sale.item_bought}</td>
                   <td className="px-4 py-2 border">${sale.total_price}</td>
                   <td className="px-4 py-2 border">{sale.quantity_bought}</td>
-                  <td className="px-4 py-2 border">{new Date(sale.order_date).toISOString().split('T')[0]}</td>
+                  <td className="px-4 py-2 border">
+                    {new Date(sale.order_date).toISOString().split("T")[0]}
+                  </td>
                 </tr>
               ))}
           </tbody>
           <tfoot className="bg-gray-50">
             <tr>
-              <td className="py-2 border">Most Active Customer: {mostActiveCustomer}</td>
-              <td className="py-2 border">Most Popular Item: {mostPopularItem}</td>
-              <td className="py-2 border">Total Earned: ${totalAmountSpent.toFixed(2)}</td>
+              <td className="py-2 border">
+                Most Active Customer: {mostActiveCustomer}
+              </td>
+              <td className="py-2 border">
+                Most Popular Item: {mostPopularItem}
+              </td>
+              <td className="py-2 border">
+                Total Earned: ${totalAmountSpent.toFixed(2)}
+              </td>
               <td className="py-2 border">Total Quantity: {totalQuantity}</td>
               <td className="py-2 border">Total Count: {numRowsShowing}</td>
             </tr>
